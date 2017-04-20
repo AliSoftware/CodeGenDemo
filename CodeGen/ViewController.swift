f1e022a21b87b401a84cd492672b089ddd1662d0
Olivier Halligon <olivier@halligon.net>
2017-04-20 13:42:58 +0200

    Initial Commit

---
+++ CodeGen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeGen
+//
+//  Created by Olivier Halligon on 20/04/2017.
+//  Copyright © 2017 AliSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
