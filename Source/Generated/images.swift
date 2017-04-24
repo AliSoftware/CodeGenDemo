d0961db0c3fd55eca2f9de4f908642303570a1f8
Olivier Halligon <olivier@halligon.net>
2017-04-24 14:01:17 +0200

    Enable FIXME warnings + manual image stencil

---
+++ Source/Generated/images.swift
@@ -0,0 +1,9 @@
+
+enum Assets: String {
+  case closeBtn = "Close-Btn"
+  case next = "Next"
+  case nsBudapest = "NSBudapest"
+  case photos = "photos"
+  case previous = "Previous"
+  case swiftGenLogo = "SwiftGen-Logo"
+}
---
+++ templates/live-image-template.sh
@@ -0,0 +1,1 @@
+kicker -e "swiftgen images Source/Assets.xcassets --templatePath templates/images.stencil -o Source/Generated/images.swift" templates/images.stencil
