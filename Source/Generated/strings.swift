ee1453d0a6532d17a93abfa65c0f6252c6a33771
Olivier Halligon <olivier@halligon.net>
2017-04-24 14:32:20 +0200

    First attempt at Strings template

---
+++ Source/Generated/strings.swift
@@ -0,0 +1,42 @@
+enum L10n: String {
+  /// Dismiss changes
+  case editAlertDismiss = "edit.alert.dismiss"
+  /// Changes have been made to the image « %@ »
+  case editAlertMessage = "edit.alert.message"
+  /// Save changes
+  case editAlertSave = "edit.alert.save"
+  /// Dismiss changes?
+  case editAlertTitle = "edit.alert.title"
+  /// Author
+  case editFieldsAuthor = "edit.fields.author"
+  /// Date
+  case editFieldsDate = "edit.fields.date"
+  /// Kind
+  case editFieldsKind = "edit.fields.kind"
+  /// Tags
+  case editFieldsTags = "edit.fields.tags"
+  /// Title
+  case editFieldsTitle = "edit.fields.title"
+  /// Edit Image
+  case editScreenTitle = "edit.screenTitle"
+  /// Welcome to %1$@, the #%2$d meetup in %3$@!
+  case homeGreetings = "home.greetings"
+  /// Slideshow
+  case homeSlideshow = "home.slideshow"
+  /// Close
+  case imageClose = "image.close"
+  /// Edit
+  case imageEdit = "image.edit"
+  /// Building
+  case imagekindBuilding = "imagekind.building"
+  /// Drawing
+  case imagekindDrawing = "imagekind.drawing"
+  /// Nature/Landscape
+  case imagekindLandscape = "imagekind.landscape"
+  /// Painting
+  case imagekindPainting = "imagekind.painting"
+  /// Face/Portrait
+  case imagekindPortrait = "imagekind.portrait"
+  /// Unspecified
+  case imagekindUnspecified = "imagekind.unspecified"
+}
---
+++ templates/live-strings-template.sh
@@ -0,0 +1,1 @@
+kicker -e "swiftgen strings Source/Base.lproj/Localizable.strings -p templates/strings.stencil -o Source/Generated/strings.swift" templates/strings.stencil
