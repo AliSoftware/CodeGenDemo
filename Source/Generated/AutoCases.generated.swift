3e35dc8df01e50e9e53cd2e383ba3bb039fe1d7a
Olivier Halligon <olivier@halligon.net>
2017-04-25 12:35:17 +0200

    Using Sourcery-provided Autocases template instead

---
+++ Source/Generated/AutoCases.generated.swift
@@ -0,0 +1,17 @@
+// Generated using Sourcery 0.6.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+extension ImageKind {
+  static var count: Int { return 6 }
+  static var allCases: [ImageKind] {
+    return [
+      .unspecified ,
+      .landscape ,
+      .building ,
+      .portrait ,
+      .drawing ,
+      .painting 
+      ]
+  }
+}
